a9753656b94b22231188e741f9005a6a44ab6a33
rafael <aguiraf@gmail.com>
2017-02-16 14:55:28 +0100

    add img

--- biplcbot.js
+++ biplcbot.js
@@ -2,18 +2,23 @@ const request = require('request')
 const TelegramBot = require('node-telegram-bot-api')
 const fs = require('fs')
 
-const token = '275404190:AAG_AklOoz82PDEgM3mylZEuPZQJU8l4dJI'      //biplcbot
+const token = process.env.TOKEN
+const fotoPath = '/tmp/biplcbot/'
 
 // Create a bot that uses 'polling' to fetch new updates
 var bot = new TelegramBot(token, { polling: true })
 bot.getMe().then(function (me) {
   console.log('Hi, my name is %s!', me.username)
+  try {
+    fs.mkdir(fotoPath, doneWriting)
+  } catch (e) {
+    if ( e.code != 'EEXIST' ) throw e;    //altrimenti ignora.. la dir esiste già
+  }
 })
 
 var msgOpts = {parse_mode : 'Markdown'}
 
 bot.onText(/(\/)?start/, function (msg, match) {
-    console.log(getUtente(msg))
     //bot.sendSticker(msg.chat.id, 'stickers/bip.webp').then(function(msg){console.log('send sticker returns',msg)})
     bot.sendSticker(msg.chat.id, 'BQADBAADLgADevHxULRnuviMixswAg')
     bot.sendMessage(msg.from.id, `*Benvenuto!*
@@ -27,17 +32,30 @@ bot.onText(/(\/)?start/, function (msg, match) {
  */
  bot.on('message', function (msg) {
        //console.log(msg)
-     if(msg.text.match(/\/start/) ) return
-     if(msg.text.match( /(\/)?id/i ) return
+     if(msg.text.match(/\/start/ ) ) return
+     if(msg.text.match(/(\/)?id/i) ) return
      var snrex = /[0-9]{9}/
      var found = msg.text.match(snrex)
      if(found){
-       getResponse(found[0], function(err, resp){
+       getResponse(found[0], msg.from.id,  function(err, resp){
          if(err){
            bot.sendMessage(msg.from.id, 'qualcosa è andato storto' , msgOpts)
            console.log(err)
          }else {
-           bot.sendMessage(msg.from.id, resp , msgOpts)
+          risposta = JSON.parse(resp).msg
+          fotoUrl = JSON.parse(resp).img
+          //console.log('resp è', JSON.parse(resp))
+          bot.sendMessage(msg.from.id, risposta , msgOpts)
+           
+          //gestione foto
+          const tmpImg = fotoPath + msg.chat.id + '.jpg'
+          request.get(fotoUrl).pipe(fs.createWriteStream(tmpImg)).on('close',
+                function(){
+                  fs.readFile(tmpImg, function (err, img)  {
+                  	if (err) return err
+                    bot.sendPhoto(msg.from.id, img)
+                  });
+                })
          }
        })
      } else {
@@ -46,11 +64,10 @@ bot.onText(/(\/)?start/, function (msg, match) {
 })
 
 
-function getResponse(sn, cb){
-  var url = 'http://odino/tesseract/index.php?option=com_jumi&fileid=91&SE=' + sn  
+function getResponse(sn, tid, cb){
+  var url = 'http://odino/tesseract/index.php?option=com_jumi&fileid=92&SE=' + sn  + '&TID=' + tid
   request.get(url, function(err, response, body) {
     if(err) return cb(err)
-    //console.log(body)
     cb(null, body)
   })  
 }
@@ -70,6 +87,5 @@ function getUtente(msg){
 }
 
 bot.onText(/(\/)?id/i, function (msg, match) {
-    console.log(getUtente(msg))
     bot.sendMessage(msg.from.id, `Il tuo Telegram ID è *${msg.from.id}*`, msgOpts)
 })
